e5d409b171b0aff71482db8833f1b3eadea79606
AndrYast <lightlimonking@gmail.com>
2015-10-14 23:21:33 +0400

    Переименование и exeшки
    mainservice теперь iuservice

--- install/install/install.cpp
+++ install/install/install.cpp
@@ -5,8 +5,12 @@
 #include <fstream>
 #include <atlstr.h>
 #include <urlmon.h>
+#include <Wincrypt.h>
 #pragma comment(lib, "urlmon.lib")
 #pragma comment(lib,"wininet")
+
+#define BUFSIZE 1024
+#define MD5LEN  16
 
 //Определение битности. Взял кусок кода отсюда: https://support.microsoft.com/en-gb/kb/2060044
 #pragma region Is64BitOperatingSystem()
@@ -73,7 +77,7 @@ int GetService()
 		HINTERNET hConnect =
 			::InternetConnect(
 			hInternet,
-			TEXT("95.213.191.87"), //сюда адрес сервера
+			TEXT("rananyev.ru"),
 			INTERNET_DEFAULT_HTTP_PORT,
 			NULL, NULL,
 			INTERNET_SERVICE_HTTP,
@@ -86,7 +90,7 @@ int GetService()
 				::HttpOpenRequest(
 				hConnect,
 				TEXT("GET"),
-				TEXT("serviceapp"),
+				TEXT("/iuservice/serviceapp/"),
 				NULL,
 				NULL,
 				0,
@@ -96,9 +100,10 @@ int GetService()
 			if (hRequest != NULL) {
 				// посылаем запрос
 				BOOL bSend = ::HttpSendRequest(hRequest, NULL, 0, NULL, 0);
+				std::cout << bSend << std::endl;
 				if (bSend) {
 					// создаём выходной файл
-					std::ofstream fnews("serviceapp.csv", std::ios::out | std::ios::binary);
+					std::ofstream fnews("serviceapp.txt", std::ios::out | std::ios::binary);
 					if (fnews.is_open()) for (;;) {
 						// читаем данные
 						char  szData[1024];
@@ -120,15 +125,18 @@ int GetService()
 						/*
 						* Тут узнаём чё откуда качать
 						*/
-						HRESULT hr = URLDownloadToFile(NULL, _T("D:/servupd.exe"/*тут ссылка на exe сервиса*/), _T("ololo.exe"), 0, NULL);
+						HRESULT hr = URLDownloadToFile(NULL, _T("https://rananyev.ru/iuservice/files/mainservice.exe"), _T("iuservice.exe"), 0, NULL); // ссылку из файла выдрать
 					}
 				}
+				else { std::cout << "req" << std::endl; std::cout << GetLastError(); }
 				// закрываем запрос
 				::InternetCloseHandle(hRequest);
 			}
+			else std::cout << "ses" << std::endl;
 			// закрываем сессию
 			::InternetCloseHandle(hConnect);
 		}
+		else std::cout << "WinInet" << std::endl;
 		// закрываем WinInet
 		::InternetCloseHandle(hInternet);
 	}
@@ -170,13 +178,133 @@ BOOL InstallAndStartService(const CString& strServiceName, const CString& strDis
 	return bResult;
 }
 
+//md5 файла
+std::string Filemd5(const CString& nameOfFile)
+{
+	std::string resmd5 = "00112233445566778899aabbccddeeff"; // костыли&велосипеды вперде
+	DWORD dwStatus = 0;
+	BOOL bResult = FALSE;
+	HCRYPTPROV hProv = 0;
+	HCRYPTHASH hHash = 0;
+	HANDLE hFile = NULL;
+	BYTE rgbFile[BUFSIZE];
+	DWORD cbRead = 0;
+	BYTE rgbHash[MD5LEN];
+	DWORD cbHash = 0;
+	CHAR rgbDigits[] = "0123456789abcdef";
+	LPCWSTR filename = (LPCTSTR)nameOfFile;
+	// Logic to check usage goes here.
+
+	hFile = CreateFile(filename,
+		GENERIC_READ,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_FLAG_SEQUENTIAL_SCAN,
+		NULL);
+
+	if (INVALID_HANDLE_VALUE == hFile)
+	{
+		dwStatus = GetLastError();
+		printf("Error opening file %s\nError: %d\n", filename,
+			dwStatus);
+		return (char*)dwStatus;
+	}
+
+	// Get handle to the crypto provider
+	if (!CryptAcquireContext(&hProv,
+		NULL,
+		NULL,
+		PROV_RSA_FULL,
+		CRYPT_VERIFYCONTEXT))
+	{
+		dwStatus = GetLastError();
+		printf("CryptAcquireContext failed: %d\n", dwStatus);
+		CloseHandle(hFile);
+		return (char*)dwStatus;
+	}
+
+	if (!CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
+	{
+		dwStatus = GetLastError();
+		printf("CryptAcquireContext failed: %d\n", dwStatus);
+		CloseHandle(hFile);
+		CryptReleaseContext(hProv, 0);
+		return (char*)dwStatus;
+	}
+
+	while (bResult = ReadFile(hFile, rgbFile, BUFSIZE,
+		&cbRead, NULL))
+	{
+		if (0 == cbRead)
+		{
+			break;
+		}
+
+		if (!CryptHashData(hHash, rgbFile, cbRead, 0))
+		{
+			dwStatus = GetLastError();
+			printf("CryptHashData failed: %d\n", dwStatus);
+			CryptReleaseContext(hProv, 0);
+			CryptDestroyHash(hHash);
+			CloseHandle(hFile);
+			return (char*)dwStatus;
+		}
+	}
+
+	if (!bResult)
+	{
+		dwStatus = GetLastError();
+		printf("ReadFile failed: %d\n", dwStatus);
+		CryptReleaseContext(hProv, 0);
+		CryptDestroyHash(hHash);
+		CloseHandle(hFile);
+		return (char*)dwStatus;
+	}
+
+	cbHash = MD5LEN;
+	if (CryptGetHashParam(hHash, HP_HASHVAL, rgbHash, &cbHash, 0))
+	{
+		//printf("MD5 hash of file %s is: ", filename);
+		for (unsigned int i = 0; i < cbHash; i++)
+		{
+			//printf("%c%c", rgbDigits[rgbHash[i] >> 4],
+				//rgbDigits[rgbHash[i] & 0xf]);
+			resmd5[2 * i] = rgbDigits[rgbHash[i] >> 4];
+			//std::cout << resmd5[2*i];
+			resmd5[2 * i + 1] = rgbDigits[rgbHash[i] & 0xf];
+		}
+		printf("\n");
+	}
+	else
+	{
+		dwStatus = GetLastError();
+		printf("CryptGetHashParam failed: %d\n", dwStatus);
+	}
+
+	CryptDestroyHash(hHash);
+	CryptReleaseContext(hProv, 0);
+	CloseHandle(hFile);
+	//std::cout << resmd5;
+	return resmd5;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int bitVer;
 	//Is64BitOperatingSystem() ? bitVer = 64 : bitVer = 32;
-	//GetService();
-	//InstallAndStartService("instupd", "instupd", "адрес exe сервиса");
-
+	GetService();
+	int timeout = 0;
+	while (Filemd5("iuservice.exe") != "c1c68651b5f5270361c84562be67af2e") //не тот хэш, надо бы из файла прочесть
+	{
+		++timeout;
+		if (timeout == 100000)
+		{
+			std::cout << "Your connection is bad";
+			break;
+		}
+	}
+	InstallAndStartService("IUservice", "IUservice", "iuservice.exe");
 	//system("pause");
 	return 0;
 }
